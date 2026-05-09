948b673e06deb7f958a79518b3b4944f85c7840a
djurslinn <djurslinnjameskm@gmail.com>
2026-05-09 18:11:13 +0530

    fixed indention

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,42 +1,37 @@
 from django.contrib import admin
+# <HINT> Import any new Models here
 from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
+# <HINT> Register QuestionInline and ChoiceInline classes here
 
-# Inline classes
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
-
 
 class ChoiceInline(admin.StackedInline):
     model = Choice
     extra = 2
 
-
 class QuestionInline(admin.StackedInline):
     model = Question
     extra = 2
 
-
-# Admin classes
+# Register your models here.
 class CourseAdmin(admin.ModelAdmin):
     inlines = [LessonInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
 
-
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
     list_display = ['content']
 
-
 class LessonAdmin(admin.ModelAdmin):
-    inlines = [QuestionInline]
     list_display = ['title']
 
+# <HINT> Register Question and Choice models here
 
-# Register models
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
